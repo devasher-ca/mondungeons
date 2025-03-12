419c6c30de781d0ddc35e174107f530cdf23cf4a
asten <0xasten@gmail.com>
2025-03-12 12:00:42 +0800

    add graph

--- graph/src/character-nft.ts
+++ graph/src/character-nft.ts
@@ -1,99 +1,49 @@
+import { Bytes } from '@graphprotocol/graph-ts'
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
   AttributePointsAssigned as AttributePointsAssignedEvent,
   CharacterCreated as CharacterCreatedEvent,
-  ExperienceGained as ExperienceGainedEvent,
-  Initialized as InitializedEvent,
-  LevelUp as LevelUpEvent,
-  NameChanged as NameChangedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Paused as PausedEvent,
-  PopulationChanged as PopulationChangedEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
-  Transfer as TransferEvent,
-  Unpaused as UnpausedEvent,
-  UpgradeAuthorized as UpgradeAuthorizedEvent,
-  Upgraded as UpgradedEvent,
-  XPManagerAdded as XPManagerAddedEvent,
-  XPManagerRemoved as XPManagerRemovedEvent
-} from "../generated/CharacterNFT/CharacterNFT"
-import {
-  Approval,
-  ApprovalForAll,
-  AttributePointsAssigned,
-  CharacterCreated,
-  ExperienceGained,
-  Initialized,
-  LevelUp,
-  NameChanged,
-  OwnershipTransferred,
-  Paused,
-  PopulationChanged,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Transfer,
-  Unpaused,
-  UpgradeAuthorized,
-  Upgraded,
-  XPManagerAdded,
-  XPManagerRemoved
-} from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+  CharacterNFT,
+} from '../generated/CharacterNFT/CharacterNFT'
+import { CharacterCreated } from '../generated/schema'
 
 export function handleAttributePointsAssigned(
-  event: AttributePointsAssignedEvent
+  event: AttributePointsAssignedEvent,
 ): void {
-  let entity = new AttributePointsAssigned(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.pointsUsed = event.params.pointsUsed
+  let tokenId = event.params.tokenId
+  let id = Bytes.fromI32(tokenId.toI32())
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let entity = CharacterCreated.load(id)
+
+  if (!entity) {
+    return
+  }
+
+  let contract = CharacterNFT.bind(event.address)
+
+  let unassignedPoints = contract.unassignedPoints(tokenId)
+  entity.unassignedPoints = unassignedPoints
+
+  let character = contract.getCharacter(tokenId)
+  entity.strength = character.attributes.strength
+  entity.dexterity = character.attributes.dexterity
+  entity.constitution = character.attributes.constitution
+  entity.intelligence = character.attributes.intelligence
+  entity.wisdom = character.attributes.wisdom
+  entity.charisma = character.attributes.charisma
 
   entity.save()
 }
 
 export function handleCharacterCreated(event: CharacterCreatedEvent): void {
-  let entity = new CharacterCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
+  let tokenId = event.params.tokenId
+  let id = Bytes.fromI32(tokenId.toI32())
+
+  let entity = CharacterCreated.load(id)
+  if (!entity) {
+    entity = new CharacterCreated(id)
+  }
+
+  entity.tokenId = tokenId
   entity.name = event.params.name
   entity.classType = event.params.classType
   entity.race = event.params.race
@@ -102,229 +52,21 @@ export function handleCharacterCreated(event: CharacterCreatedEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
-  entity.save()
-}
+  let contract = CharacterNFT.bind(event.address)
 
-export function handleExperienceGained(event: ExperienceGainedEvent): void {
-  let entity = new ExperienceGained(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.amount = event.params.amount
+  let unassignedPoints = contract.unassignedPoints(tokenId)
+  entity.unassignedPoints = unassignedPoints
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let character = contract.getCharacter(tokenId)
+  entity.strength = character.attributes.strength
+  entity.dexterity = character.attributes.dexterity
+  entity.constitution = character.attributes.constitution
+  entity.intelligence = character.attributes.intelligence
+  entity.wisdom = character.attributes.wisdom
+  entity.charisma = character.attributes.charisma
 
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleLevelUp(event: LevelUpEvent): void {
-  let entity = new LevelUp(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.newLevel = event.params.newLevel
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleNameChanged(event: NameChangedEvent): void {
-  let entity = new NameChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.newName = event.params.newName
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePopulationChanged(event: PopulationChangedEvent): void {
-  let entity = new PopulationChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.oldPopulation = event.params.oldPopulation
-  entity.newPopulation = event.params.newPopulation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgradeAuthorized(event: UpgradeAuthorizedEvent): void {
-  let entity = new UpgradeAuthorized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.newImplementation = event.params.newImplementation
-  entity.authorizer = event.params.authorizer
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleXPManagerAdded(event: XPManagerAddedEvent): void {
-  let entity = new XPManagerAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.manager = event.params.manager
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleXPManagerRemoved(event: XPManagerRemovedEvent): void {
-  let entity = new XPManagerRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.manager = event.params.manager
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.level = character.level
+  entity.xp = character.xp
 
   entity.save()
 }
